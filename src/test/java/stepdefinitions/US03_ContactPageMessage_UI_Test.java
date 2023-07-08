a39680536bc97db872d6acd3cbf160b8f3a0f857
mustafa2357 <ozkanahmet075@gmail.com>
2023-07-08 10:33:46 +0200

    andrey

---
+++ src/test/java/stepdefinitions/US03_ContactPageMessage_UI_Test.java
@@ -0,0 +1,72 @@
+package stepdefinitions;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import pages.ContactHomePage;
+import pages.ContactMessagePage;
+import utilities.ConfigReader;
+import utilities.Driver;
+import utilities.ReusableMethods;
+import utilities.WaitUtils;
+
+import static org.junit.Assert.assertTrue;
+
+public class US03_ContactPageMessage_UI_Test {
+    ContactHomePage contactHomePage =new ContactHomePage();
+    ContactMessagePage contactMessagePage=new ContactMessagePage();
+    public static String sendSuccesfull;
+    @Given("user goes to {string}")
+    public void userGoesTo(String url) {
+
+        Driver.getDriver().get(url);
+    }
+    @Given("user clicks Contact button")
+    public void user_clicks_contact_button() {
+        //managementHomePage.contactButton.click();
+        ReusableMethods.clickWithTimeOut(contactHomePage.contactButton,3);
+    }
+
+    @Given("user enters a name {string}")
+    public void user_enters_a_name(String name) {
+        //managementContactPage.yourNameBox.sendKeys(ConfigReader.getProperty());
+        contactMessagePage.yourNameBox.sendKeys(name);
+
+    }
+    @Given("user enters an email {string}")
+    public void user_enters_an_email(String email) {
+        contactMessagePage.emailBox.sendKeys(email);
+
+    }
+    @Given("user enters a subject {string}")
+    public void user_enters_a_subject(String subject) {
+        contactMessagePage.subjectBox.sendKeys(subject);
+
+    }
+    @Given("user enters a message {string}")
+    public void user_enters_a_message(String message) {
+        contactMessagePage.messageBox.sendKeys(message);
+
+    }
+    @Given("user clicks on Send Message button")
+    public void user_clicks_on_send_message_button() {
+        ReusableMethods.clickWithTimeOut(contactMessagePage.sendMessageButton,3);
+    }
+
+    @Then("assert alert message on contact page")
+    public void assertAlertMessageOnContactPage() {
+        WaitUtils.waitFor(1);
+        String alertText= contactMessagePage.sendAlert.getText();
+        assertTrue(alertText.contains("Contact Message Created Successfully"));
+
+    }
+
+    @And("close the app")
+    public void closeTheApp() {
+        WaitUtils.waitFor(3);
+      //  Driver.closeDriver();
+    }
+
+
+
+}
---
+++ src/test/resources/features/ui/US03_user_contact_message.feature
@@ -0,0 +1,17 @@
+@e2e @user_ui_test
+Feature: US03_User_can_send_a_message
+
+  Scenario Outline: send a message from Contact page
+
+    Given user goes to "https://managementonschools.com"
+    Given user clicks Contact button
+    And user enters a name "<name>"
+    And user enters an email "<email>"
+    And user enters a subject "<subject>"
+    And user enters a message "<message>"
+    And user clicks on Send Message button
+    Then assert alert message on contact page
+    And  close the app
+    Examples:
+      | name   | email                   | subject  | message               |
+      | andrey | andrey_test01@gmail.com | greeting | first message to dean |
