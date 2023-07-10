fa06dc68c743678daf85bd006cd46cd560f2f3a0
fierro <gasimovf98@gmail.com>
2023-07-10 12:29:42 -0400

    saving codes

--- src/test/java/stepdefinitions/ViceDeanStudentCreationStepDefs.java
+++ src/test/java/stepdefinitions/ViceDeanStudentCreationStepDefs.java
@@ -17,7 +17,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.Assert.assertTrue;
-import static utilities.ReusableMethods.getDropdownSelectedOptions;
+
 
 public class ViceDeanStudentCreationStepDefs {
 
@@ -58,7 +58,6 @@ public class ViceDeanStudentCreationStepDefs {
         for (WebElement w : allOptions){
             System.out.println(w.getText());
         }
-        WaitUtils.waitFor(1);
         select.selectByValue("5");
         WaitUtils.waitFor(2);
 
@@ -81,54 +80,54 @@ public class ViceDeanStudentCreationStepDefs {
         studentManagement.birthPlace.sendKeys(birthPlace);
         assertTrue(studentManagement.birthPlace.getText().contains(birthPlace));
     }
-//    @Then("enter email")
-//    public void enter_email() {
-//        // Write code here that turns the phrase above into concrete actions
-//        throw new io.cucumber.java.PendingException();
-//    }
-//    @Then("enter phone number")
-//    public void enter_phone_number() {
-//        // Write code here that turns the phrase above into concrete actions
-//        throw new io.cucumber.java.PendingException();
-//    }
-//    @Then("select gender")
-//    public void select_gender() {
-//        // Write code here that turns the phrase above into concrete actions
-//        throw new io.cucumber.java.PendingException();
-//    }
-//    @Then("select date of birth")
-//    public void select_date_of_birth() {
-//        // Write code here that turns the phrase above into concrete actions
-//        throw new io.cucumber.java.PendingException();
-//    }
-//    @Then("enter SSN respecting {string} after the 3rd and 5th digits and consist of {int} digits in total")
-//    public void enter_ssn_respecting_after_the_3rd_and_5th_digits_and_consist_of_digits_in_total(String string, Integer int1) {
-//        // Write code here that turns the phrase above into concrete actions
-//        throw new io.cucumber.java.PendingException();
-//    }
-//    @Then("enter user name")
-//    public void enter_user_name() {
-//        // Write code here that turns the phrase above into concrete actions
-//        throw new io.cucumber.java.PendingException();
-//    }
-//    @Then("enter father name")
-//    public void enter_father_name() {
-//        // Write code here that turns the phrase above into concrete actions
-//        throw new io.cucumber.java.PendingException();
-//    }
-//    @Then("enter mother name")
-//    public void enter_mother_name() {
-//        // Write code here that turns the phrase above into concrete actions
-//        throw new io.cucumber.java.PendingException();
-//    }
-//    @Then("enter password respecting password credentials")
-//    public void enter_password_respecting_password_credentials() {
-//        // Write code here that turns the phrase above into concrete actions
-//        throw new io.cucumber.java.PendingException();
-//    }
-//    @Then("verify student number automatically appear after submitting")
-//    public void verify_student_number_automatically_appear_after_submitting() {
-//        // Write code here that turns the phrase above into concrete actions
-//        throw new io.cucumber.java.PendingException();
-//    }
+    @Then("enter email")
+    public void enter_email() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("enter phone number")
+    public void enter_phone_number() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("select gender")
+    public void select_gender() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("select date of birth")
+    public void select_date_of_birth() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("enter SSN respecting {string} after the 3rd and 5th digits and consist of {int} digits in total")
+    public void enter_ssn_respecting_after_the_3rd_and_5th_digits_and_consist_of_digits_in_total(String string, Integer int1) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("enter user name")
+    public void enter_user_name() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("enter father name")
+    public void enter_father_name() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("enter mother name")
+    public void enter_mother_name() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("enter password respecting password credentials")
+    public void enter_password_respecting_password_credentials() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("verify student number automatically appear after submitting")
+    public void verify_student_number_automatically_appear_after_submitting() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
 }
--- src/test/java/utilities/ReusableMethods.java
+++ src/test/java/utilities/ReusableMethods.java
@@ -1,10 +1,11 @@
 package utilities;
 
 import org.apache.commons.io.FileUtils;
+import org.junit.Assert;
 import org.openqa.selenium.*;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.*;
-import org.testng.AssertJUnit;
+
 
 import java.io.File;
 import java.io.IOException;
@@ -18,8 +19,7 @@ import java.util.function.Function;
 
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
-import static org.testng.AssertJUnit.assertEquals;
-import static org.testng.AssertJUnit.assertFalse;
+
 
 public class ReusableMethods {
 
@@ -209,6 +209,226 @@ public class ReusableMethods {
                 break;
             }
         }
+    }
+
+
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+    //    RADIO BUTTON
+    public static void clickRadioByIndex(int index){
+        int numOfRadio =Driver.getDriver().findElements(By.xpath("//input[@type='radio']")).size();
+        for (int i=0;i<numOfRadio;i++){
+            if (!Driver.getDriver().findElements(By.xpath("//input[@type='radio']")).get(index).isSelected()) {
+                Driver.getDriver().findElements(By.xpath("//input[@type='radio']")).get(index).click();
+            }
+        }
+    }
+
+    //    CHECKBOX BUTTON
+    public static void clickCheckboxByIndex(int index){
+        int numOfRadio =Driver.getDriver().findElements(By.xpath("//input[@type='checkbox']")).size();
+        try{
+            for (int i=0;i<numOfRadio;i++){
+                if (!Driver.getDriver().findElements(By.xpath("//input[@type='checkbox']")).get(index).isSelected()) {
+                    Driver.getDriver().findElements(By.xpath("//input[@type='checkbox']")).get(index).click();
+                }
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+    //    DROPDOWN
+
+    public static void selectByIndex(WebElement element, int index){
+        Select objSelect =new Select(element);
+        objSelect.selectByIndex(index);
+    }
+
+    public static void selectByValue(WebElement element, String value) {
+        Select objSelect = new Select(element);
+        objSelect.selectByValue(value);
+    }
+
+    public static void selectDropdownByValue(WebElement element,String textOfDropdown){
+        List<WebElement> options = element.findElements(By.tagName("option"));
+        for (WebElement option : options){
+            System.out.println(option.getText());
+            if (option.getText().equals(textOfDropdown)){
+                option.click();
+                break;
+            }
+        }
+    }
+    //  DROPDOWN
+    /**
+     * Selects a random value from a dropdown list and returns the selected Web Element
+     * @param
+     * @return
+     */
+
+
+    //    DROPDOWN: accepts dropdown element and returns all selected element texts as an arraylist
+    public static ArrayList<String> getDropdownSelectedOptions(WebElement element) throws Exception {
+        if (element!=null){
+            Select list = new Select(element);
+            ArrayList<WebElement> allSelectedOptions = (ArrayList<WebElement>) list.getAllSelectedOptions();
+            ArrayList<String> result = new ArrayList<String>();
+            for (WebElement eachSelected : allSelectedOptions){
+                result.add(eachSelected.getText());
+            }
+            return result;
+        }else {
+            throw new Exception("No element is returned");
+        }
+    }
+
+    //    VERIFY ELEMENT IS DISPLAYED
+    /**
+     * Verifies whether the element is displayed on page
+     * fails if the element is not found or not displayed
+     *
+     * @param element
+     */
+    public static void verifyElementDisplayed(WebElement element) {
+        try {
+            assertTrue("Element is not visible: " + element, element.isDisplayed());
+        } catch (NoSuchElementException e) {
+            Assert.fail("Element is not found: " + element);
+        }
+    }
+
+    /**
+     * Verifies whether the element matching the provided locator is displayed on page
+     * fails if the element matching the provided locator is not found or not displayed
+     *
+     * @param by
+     */
+    public static void verifyElementDisplayed(By by) {
+        try {
+            assertTrue("Element not visible: " + by, Driver.getDriver().findElement(by).isDisplayed());
+        } catch (NoSuchElementException e) {
+            Assert.fail("Element not found: " + by);
+        }
+    }
+
+    //VERIFY ELEMENT IS NOT DISPLAYED
+    /**
+     * Verifies whether the element matching the provided locator is NOT displayed on page
+     * fails if the element matching the provided locator is not found or not displayed
+     *
+     * @param by
+     */
+    public static void verifyElementNotDisplayed(By by) {
+        try {
+            Assert.assertFalse("Element should not be visible: " + by, Driver.getDriver().findElement(by).isDisplayed());
+        } catch (NoSuchElementException e) {
+            e.printStackTrace();
+        }
+    }
+    /**
+     * Verifies whether the element matching the provided WebElement is NOT displayed on page
+     * fails if the element matching the WebElement is not found or not displayed
+     * @paramWebElement
+     */
+    public static void verifyElementNotDisplayed(WebElement element) {
+        try {
+            Assert.assertFalse("Element should not be visible: " + element, element.isDisplayed());
+        } catch (NoSuchElementException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void verifyElementClickable(WebElement element) {
+        try {
+            Assert.assertTrue("Element not visible: " + element, element.isEnabled());
+        } catch (NoSuchElementException e) {
+            e.printStackTrace();
+            Assert.fail("Element not found: " + element);
+
+        }
+    }
+    public static void verifyElementNotClickable(WebElement element) {
+        try {
+            Assert.assertFalse("Element not visible: " + element, element.isEnabled());
+        } catch (NoSuchElementException e) {
+            e.printStackTrace();
+        }
+    }
+
+    //    ALERT
+    public static void acceptAlert() throws InterruptedException {
+        Driver.getDriver().switchTo().alert().accept();
+    }
+    public static void dismissAlert() throws InterruptedException {
+        Driver.getDriver().switchTo().alert().accept();
+    }
+    //    IFRAME
+    public static void switchIframeByWebElement(String xpath){
+        WebElement iframeElement = Driver.getDriver().findElement(By.xpath(xpath));
+        Driver.getDriver().switchTo().frame(iframeElement);
+    }
+    //    IFRAME
+    public static void switchIframeByIndex(int index){
+        Driver.getDriver().switchTo().frame(index);
+    }
+    //    MULTIPLE WINDOW !!!
+    public static void switchToWindowByTitle(String targetTitle) {
+        String origin = Driver.getDriver().getWindowHandle();
+        for (String childWindow : Driver.getDriver().getWindowHandles()) {
+            Driver.getDriver().switchTo().window(childWindow);
+            if (Driver.getDriver().getTitle().equals(targetTitle)) {
+                System.out.println("Switched to the window : "+targetTitle);
+                return;
+            }
+        }
+        Driver.getDriver().switchTo().window(origin);
+    }
+    //    windowNumber starts at (0)
+    public static void switchToWindow(int windowNumber){
+        List<String> list = new ArrayList<>(Driver.getDriver().getWindowHandles());
+        Driver.getDriver().switchTo().window(list.get(windowNumber));
+    }
+    //    ACTIONS_RIGHT CLICK
+    public static void rightClickOnElementActions(WebElement element) {
+        Actions actions = new Actions(Driver.getDriver());
+        actions.contextClick(element).perform();
+    }
+    //ACTIONS_DOUBLE CLICK
+
+    //    ACTIONS_HOVER_OVER
+    public static void hoverOverOnElementActions(WebElement element) {
+        //        Actions actions = new Actions(driver);
+        new Actions(Driver.getDriver()).moveToElement(element).perform();
+    }
+    //    ACTIONS_SCROLL_DOWN
+    public static void scrollDownActions() {
+        //        Actions actions = new Actions(driver);
+        new Actions(Driver.getDriver()).sendKeys(Keys.PAGE_DOWN).perform();
+    }
+    //    ACTIONS_SCROLL_UP
+    public static void scrollUpActions() {
+        //        Actions actions = new Actions(driver);
+        new Actions(Driver.getDriver()).sendKeys(Keys.PAGE_UP).perform();
+    }
+    //    ACTIONS_SCROLL_RIGHT
+    public static void scrollRightActions(){
+        new Actions(Driver.getDriver()).sendKeys(Keys.ARROW_RIGHT).sendKeys(Keys.ARROW_RIGHT).perform();
+    }
+    //    ACTIONS_SCROLL_LEFT
+    public static void scrollLeftActions(){
+        new Actions(Driver.getDriver()).sendKeys(Keys.ARROW_LEFT).sendKeys(Keys.ARROW_LEFT).perform();
+    }
+    //    ACTIONS_DRAG_AND_DROP
+    public static void dragAndDropActions(WebElement source, WebElement target) {
+        //        Actions actions = new Actions(driver);
+        new Actions(Driver.getDriver()).dragAndDrop(source,target).perform();
+    }
+    //    ACTIONS_DRAG_AND_DROP_BY
+    public static void dragAndDropActions(WebElement source, int x, int y) {
+        //        Actions actions = new Actions(driver);
+        new Actions(Driver.getDriver()).dragAndDropBy(source,x,y).perform();
     }
 }
 
