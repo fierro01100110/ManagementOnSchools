ce6240d57729a174ea6e6c2e2e1acf53ad79d2c0
selda75 <127150726+selda75@users.noreply.github.com>
2023-07-27 16:17:50 -0600

    us24 updated

--- src/test/java/runners/Selda_Runner.java
+++ src/test/java/runners/Selda_Runner.java
@@ -20,10 +20,10 @@ import org.junit.runner.RunWith;
 
         monochrome = true,
 
-        features = "./src/test/resources/features/ui",
+        features = "./src/test/resources/features",
         glue = {"stepdefinitions","hooks"},
         dryRun = false,
-        tags = "@US24"
+        tags = "@US24_api_test"
 )
 
 
