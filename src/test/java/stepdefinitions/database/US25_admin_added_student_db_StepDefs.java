9953b98affb3a75bf04d6870632077a316ed67c7
Hakan MOllaoglu <hakanmollaoglu@gmail.com>
2023-07-28 11:09:58 -0400

    hakan

--- src/test/java/stepdefinitions/database/US25_admin_added_student_db_StepDefs.java
+++ src/test/java/stepdefinitions/database/US25_admin_added_student_db_StepDefs.java
@@ -23,26 +23,31 @@ public class US25_admin_added_student_db_StepDefs {
         resultSet = getResultSet(sqlQuery);
 
     }
-    @Then("body contains username {string}, birth_day {string}, birth_place {string},gender {string}, name {string},phone_number {string}, ssn {string},surname {string}")
-    public void bodyContainsUsernameBirth_dayBirth_placeGenderNamePhone_numberSsnSurname(String username, String birth_day, String birth_place, String gender, String name, String phone_number, String ssn, String surname) throws SQLException {
+    @Then("body should contain userId {string}, username {string}, name {string}, surname {string}, birthDay {string}, birthPlace {string}, phoneNumber {string}, gender {string}, motherName {string}, fatherName {string}, email {string}")
+    public void bodyShouldContainUserIdUsernameNameSurnameBirthDayBirthPlacePhoneNumberGenderMotherNameFatherNameEmail(String userId, String username, String name, String surname, String birthDay, String birthPlace, String phoneNumber, String gender, String motherName, String fatherName, String email) throws SQLException {
+
+
+        //@Then("body contains username {string}, birth_day {string}, birth_place {string},gender {string}, name {string},phone_number {string}, ssn {string},surname {string}")
+    //public void bodyContainsUsernameBirth_dayBirth_placeGenderNamePhone_numberSsnSurname(String username, String birth_day, String birth_place, String gender, String name, String phone_number, String ssn, String surname) throws SQLException {
         // Write code here that turns the phrase above into concrete actions
         resultSet.next();
 
-        String actBirth_day = resultSet.getString("birth_day");
-        String actBirth_place = resultSet.getString("birth_place");
+        String actBirth_day = resultSet.getString("birthDay");
+        String actBirth_place = resultSet.getString("birthPlace");
         String actGender = resultSet.getString("gender");
         String actName = resultSet.getString("name");
-        String actPhone_number = resultSet.getString("phone_number");
-        String actSsn = resultSet.getString("ssn");
+        String actPhone_number = resultSet.getString("phoneNumber");
         String actSurname = resultSet.getString("surname");
+        String actMotherName = resultSet.getString("motherName");
+        String actFatherName = resultSet.getString("fatherName");
+        String actEmail = resultSet.getString("email");
         String actUsername = resultSet.getString("username");
 
-        assertEquals(birth_day, actBirth_day);
-        assertEquals(birth_place, actBirth_place);
+        assertEquals(birthDay, actBirth_day);
+        assertEquals(birthPlace, actBirth_place);
         assertEquals(gender, actGender);
         assertEquals(name, actName);
-        assertEquals(phone_number, actPhone_number);
-        assertEquals(ssn, actSsn);
+        assertEquals(phoneNumber, actPhone_number);
         assertEquals(surname, actSurname);
         assertEquals(username, actUsername);
 
